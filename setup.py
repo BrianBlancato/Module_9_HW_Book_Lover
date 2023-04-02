58c5292a7d9a751e1633883818b5b2e77472564a
BrianAnthony_Blancato <bab3fq@virginia.edu>
2023-04-02 14:34:15 -0400

    Final Updates

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='booklover',
     version='1.0.0',
-    url='',
+    url='https://github.com/BrianBlancato/Module_9_HW_Book_Lover',
     author='Brian Blancato',
     author_email='bab3fq@virginia.com',
     description='This is a book lover package that tracks the books a person has read',
